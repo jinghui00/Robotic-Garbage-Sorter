6eb447b4a3de00338712be4668bad9bfecd4f518
jinghui00 <60105748+jinghui00@users.noreply.github.com>
2023-02-03 00:03:35 +0800

    Update gripper_publisher.py

--- dofbot_moveit/scripts/gripper_publisher.py
+++ dofbot_moveit/scripts/gripper_publisher.py
@@ -14,21 +14,25 @@ def gripper_publisher():
     # Create the publisher object
     pub = rospy.Publisher('gripper_topic', Float64, queue_size=10)
 
+    # Read the angle of joint 6 (gripper)
     msg = Float64()
     angle = sbus.Arm_serial_servo_read(6)
     print("Angle=", angle)
+    
     # Make sure the angle is not None before using it in the calculation
     if angle is not None:
-        if angle <= 60.0:
-            msg.data = 140.0
-        elif angle > 60.0 and angle <= 180.0:
-            msg.data = 30.0
+        if angle <= 60.0: # If  gripper is open
+            msg.data = 140.0 # Close the gripper
+        elif angle > 60.0 and angle <= 180.0: # If gripper is close
+            msg.data = 30.0 # Open the gripper
     else:
         # Handle the case where the angle is None
         print("Error: Angle is not defined")
-    rospy.sleep(5)
+        
+    rospy.sleep(5) # to enable the gripper_subscriber node has enough time to start the program and receive the message
     pub.publish(msg)
     print("done publish gripper")
+    
     et = time.time()
     elapsed_time = et-st
     print('Execution time : ',elapsed_time,'seconds')
