d17d5adca24ea24a0abfd402371968194c863cc1
jinghui00 <60105748+jinghui00@users.noreply.github.com>
2023-02-03 00:23:46 +0800

    Update run.sh

--- dofbot_moveit/scripts/run.sh
+++ dofbot_moveit/scripts/run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 ./detection_bin.sh
 sleep 15
+# to run garbage classification twice, can add more or do a for loop to run multiple times
 ./detection_garbage.sh
 sleep 15
 ./detection_garbage.sh
