3a9e933dbe7ac7a757352d3d5984e574bd73fb95
jinghui00 <60105748+jinghui00@users.noreply.github.com>
2023-02-03 00:05:19 +0800

    Update gripper_publisher.py

--- dofbot_moveit/scripts/gripper_publisher.py
+++ dofbot_moveit/scripts/gripper_publisher.py
@@ -30,7 +30,7 @@ def gripper_publisher():
         print("Error: Angle is not defined")
         
     rospy.sleep(5) # to enable the gripper_subscriber node has enough time to start the program and receive the message
-    pub.publish(msg)
+    pub.publish(msg) # publish the value of angle
     print("done publish gripper")
     
     et = time.time()
