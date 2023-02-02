c0ce0c9d129cdf9a8575c809f53f421c0d75e88a
jinghui00 <60105748+jinghui00@users.noreply.github.com>
2023-02-03 00:21:07 +0800

    Update arm_move_leftright.py

--- dofbot_moveit/scripts/arm_move_leftright.py
+++ dofbot_moveit/scripts/arm_move_leftright.py
@@ -4,7 +4,6 @@ import time
 from time import sleep
 import rospy
 from moveit_commander.move_group import MoveGroupCommander
-#import Arm_Lib
 from Arm_Lib import Arm_Device
 from signal import pthread_kill, SIGTSTP
 from math import pi
@@ -18,43 +17,50 @@ RA2DE = 180 / pi
 def topic(msg):
    
     if not isinstance(msg, JointState): return
-    #90 is open, 180 is close
+    # 30 is open, 140 is close
     joints = [0.0, 0.0, 0.0, 0.0, 0.0, 140.0]
    
     for i in range(5): joints[i] = (msg.position[i] * RA2DE) + 90
   
+    # Set joints position
     sbus.Arm_serial_servo_write6_array(joints, 1000)
 
-def move_left():
+def move_left(): # Arm at left side
     print("move to left")
-    #dofbot.set_joint_value_target([-1.49, 0.69, -0.83, -1.57, -0.00])
-    dofbot.set_joint_value_target([0.00, 0.00, 0.00, -1.57, -0.00])
-    dofbot.go()
+      
+    # Set new joints position: arm upwards
+    dofbot.set_joint_value_target([0.00, 0.00, 0.00, -1.57, -0.00]) 
+    dofbot.go() # Perform action
+    sleep(0.5)
+      
+    # Set new joints position: arm turn left
+    dofbot.set_joint_value_target([-1.49, 0.00, 0.00, -1.57, -0.00]) 
+    dofbot.go() # Perform action
     sleep(0.5)
     
-    dofbot.set_joint_value_target([-1.49, 0.00, 0.00, -1.57, -0.00])
-    dofbot.go()
-    sleep(0.5)
-    
-def move_right():
+def move_right(): # Arm at right side
     print("move to right")
-    #dofbot.set_joint_value_target([1.49,0.69,-0.83, -1.57, -0.00])
-    dofbot.set_joint_value_target([0.00, 0.00, 0.00, -1.57, -0.00])
-    dofbot.go()
+      
+    # Set new joints position: arm upwards
+    dofbot.set_joint_value_target([0.00, 0.00, 0.00, -1.57, -0.00]) 
+    dofbot.go() # Perform action
+    sleep(0.5)
+      
+    # Set new joints position: arm turn right
+    dofbot.set_joint_value_target([1.49, 0.00, 0.00, -1.57, -0.00]) 
+    dofbot.go() # Perform action
     sleep(0.5)
     
-    dofbot.set_joint_value_target([1.49, 0.00, 0.00, -1.57, -0.00])
-    dofbot.go()
-    sleep(0.5)
-    
-def callback(data):
+def callback(data): # Receive the message
     if data.data == "paper":
         print("Subscribed paper")
         move_left()
+         
     elif data.data == "plastic":
         print("Subscribed plastic")
         move_right()
         
+         
 if __name__ == '__main__':
 
     rospy.init_node("dofbot_set_move_side")
@@ -80,6 +86,6 @@ if __name__ == '__main__':
     sbus = Arm_Device()
     time.sleep(.1)
     rospy.Subscriber("/joint_states", JointState, topic)
+    # Create another subscriber object to receive the garbage classification result
     rospy.Subscriber("arm_node", String, callback)
-    
     rospy.spin()
